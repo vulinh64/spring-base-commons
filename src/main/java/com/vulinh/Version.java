466964dd44beab1ba36d0e3fcaa30c63472645dd
Linh Nguyen <nguyenvulinh64@outlook.com>
2025-12-15 18:00:53 +0700

    Add JPA equals and hashCode utils

--- src/main/java/com/vulinh/Version.java
+++ src/main/java/com/vulinh/Version.java
@@ -8,5 +8,5 @@ final class Version {
   // No instantiation
   private Version() {}
 
-  static final String VERSION = "1.0.0";
+  static final String VERSION = "1.0.1";
 }
---
+++ src/main/java/com/vulinh/data/entity/AbstractIdentifiable.java
@@ -0,0 +1,40 @@
+package com.vulinh.data.entity;
+
+import com.vulinh.data.Identifiable;
+import java.io.Serializable;
+import java.util.Objects;
+import org.hibernate.proxy.HibernateProxy;
+
+// https://jpa-buddy.com/blog/hopefully-the-final-article-about-equals-and-hashcode-for-jpa-entities-with-db-generated-ids/
+public abstract class AbstractIdentifiable<I extends Serializable> implements Identifiable<I> {
+
+  @Override
+  public final boolean equals(Object other) {
+    if (this == other) {
+      return true;
+    }
+
+    var id = getId();
+
+    if (id == null) {
+      return false;
+    }
+
+    return other instanceof AbstractIdentifiable<?> ai
+        && getEffectiveClass(this) == getEffectiveClass(ai)
+        && Objects.equals(id, ai.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    var id = getId();
+
+    return id == null ? getEffectiveClass(this).hashCode() : id.hashCode();
+  }
+
+  static Class<?> getEffectiveClass(Object object) {
+    return object instanceof HibernateProxy hibernateProxy
+        ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
+        : object.getClass();
+  }
+}
